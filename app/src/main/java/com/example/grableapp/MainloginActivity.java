24614f0334d196ee4e20d089ef8d997b4f508cb2
AndreeaPloscar <40761151+AndreeaPloscar@users.noreply.github.com>
2019-09-12 17:42:33 +0300

    Grable-design welcome

--- app/src/main/java/com/example/grableapp/MainloginActivity.java
+++ app/src/main/java/com/example/grableapp/MainloginActivity.java
@@ -6,16 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainloginActivity extends AppCompatActivity {
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainloginActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    Button login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainlogin);
+        Firebase.setAndroidContext(this);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+
+            Intent intent = new Intent(MainloginActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+
+        }
 
 
-        Button login_btn = findViewById(R.id.firstpagelogin);
-
+        login_btn =(Button)findViewById(R.id.firstpagelogin);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,3 +45,5 @@ public class MainloginActivity extends AppCompatActivity {
     }
 
 }
+
+
