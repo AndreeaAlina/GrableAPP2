42f9a6c6b1390adc6b6ca592014083181d2c65f0
Andreiutz <andreicatineanu12@gmail.com>
2019-09-24 23:34:55 +0300

    bifa la payment

--- app/src/main/java/com/example/grableapp/ListActivity.java
+++ app/src/main/java/com/example/grableapp/ListActivity.java
@@ -1,13 +1,20 @@
 package com.example.grableapp;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +37,11 @@ public class ListActivity extends AppCompatActivity  {
     Button add_btn;
     Button finish_btn;
     TextView text_view;
+    ImageView done;
+    ImageView done_circle;
+    AnimatedVectorDrawableCompat avd;
+    AnimatedVectorDrawable avd2;
+    TextView thank_you_text;
 
     private ListView lvProduct;
     private ProductListAdapter adapter;
@@ -61,6 +73,27 @@ public class ListActivity extends AppCompatActivity  {
         add_btn = findViewById(R.id.list_btn);
         finish_btn = findViewById(R.id.finish_shopping);
         text_view = findViewById(R.id.textviewlist);
+        done = findViewById(R.id.done);
+        done_circle = findViewById(R.id.markfundal);
+        thank_you_text = findViewById(R.id.thankyoutext);
+
+
+        final ImageView loading_animation = findViewById(R.id.loadinganimation);
+        final ImageView payment_view = findViewById(R.id.animationview);
+        final TextView plsw_wait = findViewById(R.id.pleasewaitvew);
+        final TextView processing = findViewById(R.id.proccesingview);
+
+        loading_animation.setAlpha(1f);
+        payment_view.setAlpha(1f);
+        plsw_wait.setAlpha(1f);
+        processing.setAlpha(1f);
+
+        loading_animation.animate().translationYBy(-1000f);
+        payment_view.animate().translationYBy(-1000f);
+        plsw_wait.animate().translationYBy(-1000f);
+        processing.animate().translationYBy(-1000f);
+        thank_you_text.animate().translationYBy(1000);
+
 
 
 
@@ -136,14 +169,74 @@ public class ListActivity extends AppCompatActivity  {
                         build2.setTitle("Choose payment method");
                         build2.setPositiveButton("Credit card", new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
+                            public void onClick(final DialogInterface dialogInterface, int i) {
                                 for (Product item : mProductList) {
                                     total += item.getPrice();
-                                    Intent intent = new Intent(ListActivity.this, CardPaymentActivity.class);
-                                    startActivity(intent);
+                                    //Intent intent = new Intent(ListActivity.this, CardPaymentActivity.class);
+                                    //startActivity(intent);
                                 }
                                 String totall = total + "";
-                                Toast.makeText(ListActivity.this, "Mai mult cod", Toast.LENGTH_SHORT).show();
+                                lvProduct.setAlpha(0f);
+                                add_btn.setAlpha(0f);
+                                finish_btn.setAlpha(0f);
+                                //Handler handler2 = new Handler();
+                                loading_animation.animate().translationYBy(1000f).setDuration(500);
+                                payment_view.animate().translationYBy(1000f).setDuration(500);
+                                plsw_wait.animate().translationYBy(1000f).setDuration(500);
+                                processing.animate().translationYBy(1000f).setDuration(500);
+
+
+                                Handler handler2 = new Handler();
+                                handler2.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        loading_animation.animate().rotation(3600).setDuration(5000);
+                                    }
+                                }, 500);
+
+                                //loading_animation.animate().rotation(3600).setDuration(5000);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Do something after 5s = 5000ms
+                                        loading_animation.setAlpha(0f);
+                                        payment_view.setAlpha(0f);
+                                        plsw_wait.setAlpha(0f);
+                                        processing.setAlpha(0f);
+
+                                    }
+                                }, 7000);
+
+                                Handler handler3 = new Handler();
+                                handler3.postDelayed(new Runnable() {
+
+                                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                                    @Override
+                                    public void run() {
+                                        done_circle.setAlpha(1f);
+                                        Drawable drawable = done.getDrawable();
+                                        if (drawable instanceof AnimatedVectorDrawableCompat)
+                                        {
+                                            avd = (AnimatedVectorDrawableCompat) drawable;
+                                            avd.start();
+                                        }
+                                        else if (drawable instanceof AnimatedVectorDrawable)
+                                        {
+                                            avd2 = (AnimatedVectorDrawable) drawable;
+                                            avd2.start();
+                                        }
+                                    }
+                                }, 7500);
+
+                                Handler handler4 = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        thank_you_text.animate().translationYBy(-1000).setDuration(300);
+                                    }
+                                }, 8000);
+
                             }
                         });
                         build2.setNegativeButton("Generate QR code", new DialogInterface.OnClickListener() {
