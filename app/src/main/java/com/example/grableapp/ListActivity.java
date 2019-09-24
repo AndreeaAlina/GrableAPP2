3e6285cf910d041d5b41d1068457748759b46bd4
Andreiutz <andreicatineanu12@gmail.com>
2019-09-24 19:52:32 +0300

    loading screen la payment

--- app/src/main/java/com/example/grableapp/ListActivity.java
+++ app/src/main/java/com/example/grableapp/ListActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -28,10 +29,12 @@ public class ListActivity extends AppCompatActivity  {
 
     Button add_btn;
     Button finish_btn;
+    TextView text_view;
 
     private ListView lvProduct;
     private ProductListAdapter adapter;
     private List<Product> mProductList;
+    private int number_of_products = 0;
     private double total = 0.0;
 
 
@@ -57,6 +60,9 @@ public class ListActivity extends AppCompatActivity  {
 
         add_btn = findViewById(R.id.list_btn);
         finish_btn = findViewById(R.id.finish_shopping);
+        text_view = findViewById(R.id.textviewlist);
+
+
 
         lvProduct = (ListView)findViewById(R.id.listview_produsct);
 
@@ -65,19 +71,19 @@ public class ListActivity extends AppCompatActivity  {
 
         //Add items to list
         //Se poate din database
-
-        mProductList.add(new Product(1, "iPhone4", 200, "Apple iPhone4 16GB"));
-        mProductList.add(new Product(2, "iPhone5", 400, "Apple iPhone5 16GB"));
-        mProductList.add(new Product(3, "iPhone5s", 500, "Apple iPhone5s 16GB"));
-        mProductList.add(new Product(4, "iPhone6", 700, "Apple iPhone6 16GB"));
+/*
+        mProductList.add(new Product(1, "Kefir", 12, "Covalact, 20% grasime"));
+        mProductList.add(new Product(2, "Tastatura Asus", 300, "Tastatura Gaming Asus Cerberus"));
+        mProductList.add(new Product(3, "iPhone5s", 1000, "Apple iPhone5s 16GB"));
+        mProductList.add(new Product(4, "Paine VelPitar", 3.5, "Paine VelPitar Alba feliata"));
         mProductList.add(new Product(5, "iPhone6s", 1000, "Apple iPhone6s 16GB"));
         mProductList.add(new Product(6, "iPhone7", 1500, "Apple iPhone7 16GB"));
         mProductList.add(new Product(7, "iPhone7Plus", 2000, "Apple iPhone7Plus 16GB"));
         mProductList.add(new Product(8, "iPhone8", 2500, "Apple iPhone8 16GB"));
         mProductList.add(new Product(9, "iPhoneX", 4000, "Apple iPhoneX 16GB"));
         mProductList.add(new Product(10, "iPhone11", 6000, "Apple iPhone11 16GB"));
-        addItemToList("Carne", 20.5, "Pui");
-
+        addItemToList("Carne", 20.5, "Piept de pui");
+*/
 
         ///more to add
 
@@ -100,7 +106,11 @@ public class ListActivity extends AppCompatActivity  {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ListActivity.this, ScannerActivity.class);
-                startActivity(intent);
+                //startActivity(intent);
+                mProductList.add(new Product(1, "Kefir", 12, "Covalact, 20% grasime"));
+                number_of_products++;
+                text_view.setAlpha(0f);
+                adapter.notifyDataSetChanged();
             }
         });
 
@@ -129,7 +139,7 @@ public class ListActivity extends AppCompatActivity  {
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 for (Product item : mProductList) {
                                     total += item.getPrice();
-                                    Intent intent = new Intent(ListActivity.this, SettingsActivity.class);
+                                    Intent intent = new Intent(ListActivity.this, CardPaymentActivity.class);
                                     startActivity(intent);
                                 }
                                 String totall = total + "";
